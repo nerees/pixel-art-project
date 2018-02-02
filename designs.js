7ae14eee4cf42631e58ab2f5113fffc34174ab8a
Nerijus Latakas <32032495+nerees@users.noreply.github.com>
2018-02-02 15:43:01 +0200

    Add files via upload

--- designs.js
+++ designs.js
@@ -4,14 +4,14 @@ $( document ).ready(function() {
 //waiting till the height and width of the grid will be submitted
 document.getElementById('sizePicker').addEventListener('submit', function(evt){
       evt.preventDefault(); // without this grid dissapears
-      var h = document.getElementById('inputHeight').value;
-      var w = document.getElementById('inputWeight').value;
+      let h = document.getElementById('inputHeight').value;
+      let w = document.getElementById('inputWeight').value;
       makeGrid(h, w); //calling a function whitch draws a grid
 });
 // when the grid squere is clicked
 $('#pixelCanvas').on('click', 'td', function(evt){
       // getting the collor code. It can be different every click
-      var color = document.getElementById('colorPicker').value;
+      let color = document.getElementById('colorPicker').value;
       console.log(color);
       if ($(this).hasClass('sqr')){
           $(this).toggleClass('sqr');
@@ -24,15 +24,20 @@ $('#pixelCanvas').on('click', 'td', function(evt){
 });
 //drawing a grid from subbmited width and height.
 function makeGrid(h, w) {
-    var table = $('#pixelCanvas');
+    const table = $('#pixelCanvas');
     table.find('tbody').remove();
     table.append('<tbody></tbody>');
-    var grid = table.find('tbody');
-      for (var i = 0; i < h; i++){
+    const grid = table.find('tbody');
+      for (let i = 0; i < h; i++){
         grid.append('<tr></tr>');
       }
-      for (var j = 0; j < w; j++){
+      for (let j = 0; j < w; j++){
         table.find('tr').append('<td class="sqr"></td>');
       }
+    const note = $('#note');
+    note.find('p').remove();
+    note.append('<p></p>');
+    const notetext = note.find('p');
+    notetext.append('To clear the color please click again on grid');  
 
 }
